89fc620e7e1a73ab08cce1d816c4d0ce4096ae0b
Tony Phan <phantj@uci.edu>
2016-10-15 15:27:55 -0700

    Created a basic player character (currently a circle with a directional arrow) that can move forwards and backwards and rotate left and right thru the WASD/arrow keys :) You can test it out in the scene named playerTest

---
+++ Pirates/Assets/Scripts/basicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+
+		transform.Rotate(0, 0, -x);
+		transform.Translate(0, z, 0);
+	}
+}
