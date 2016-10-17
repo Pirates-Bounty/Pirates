2b7e7a932c2cb97fdf2261862ff058fcf4d111ac
Quinn Levine <Qlevine@uci.edu>
2016-10-17 16:04:14 -0700

    Basic tile map created with land and water tiles.  For now generation is random but we will change this later.

---
+++ Pirates/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int width;
+    public int height;
+    public Sprite[] sprites;
+    public Color[] colors;
+    private Transform canvas;
+    private TileMap tileMap;
+    // Use this for initialization
+    void Start () {
+        canvas = GameObject.Find("Canvas").transform;
+        tileMap = new TileMap(width, height,sprites.Length);
+        tileMap.Generate();
+        Render();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Render()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                UI.CreatePanel("(" + i + ", " + j + ")", sprites[tileMap.map[i, j]], colors[tileMap.map[i, j]], canvas, Vector3.zero,
+                   new Vector2((1.0f / width) * i, (1.0f / height) * j), new Vector2((1.0f / width) * (i + 1), (1.0f / height) * (j + 1)));
+            }
+        }
+    }
+}
---
+++ Pirates/Assets/Scripts/TileMap.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMap {
+    public GameObject[] tiles;
+    private int width;
+    private int height;
+    private int uniqueElements;
+    public int[,] map;
+    public TileMap(int width, int height,int uniqueElements)
+    {
+        this.width = width;
+        this.height = height;
+        this.uniqueElements = uniqueElements;
+        map = new int[width, height];
+    }
+	
+    public void Generate()
+    {
+        for(int i = 0; i < width; ++i)
+        {
+            for(int j = 0; j < height; ++j)
+            {
+                map[i, j] = Random.Range(0, uniqueElements);
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
