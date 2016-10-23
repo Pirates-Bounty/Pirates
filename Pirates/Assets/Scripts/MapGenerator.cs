f2b57b004cd6e862cb33b85b54addfbed0f53cdf
Quinn Levine <Qlevine@uci.edu>
2016-10-22 18:03:44 -0700

    Using noise John and I updated the generator.  We have seed generation as well and the map can be updated in game using r.

--- Pirates/Assets/Scripts/MapGenerator.cs
+++ Pirates/Assets/Scripts/MapGenerator.cs
@@ -5,31 +5,122 @@ public class MapGenerator : MonoBehaviour {
 
     public int width;
     public int height;
+    public float frequency;
     public Sprite[] sprites;
     public Color[] colors;
+    public int seed;
+    public string[] tileNames;
     private Transform canvas;
-    private TileMap tileMap;
+    public int[,] map;
+
     // Use this for initialization
     void Start () {
         canvas = GameObject.Find("Canvas").transform;
-        tileMap = new TileMap(width, height,sprites.Length);
-        tileMap.Generate();
-        Render();
-	}
+        Generate();
+        GenerateGameObjects();
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            DeleteChildren();
+            Generate();
+            GenerateGameObjects();
+        }
+
 	
 	}
 
-    void Render()
+    void DeleteChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+
+    public void Generate()
+    {
+
+        Random.InitState(seed);
+        float xOffset = Random.Range(-100000, 100000);
+        float yOffset = Random.Range(-100000, 100000);
+
+
+        map = new int[width, height];
+
+        for (int i = 0; i < width; ++i)
+        {
+            for (int j = 0; j < height; ++j)
+            {
+                //Make this less random
+                float x = (float)i * frequency / 1000f;
+                float y = (float)j * frequency / 1000f;
+                float noise = Mathf.PerlinNoise(x + xOffset, y + yOffset);
+
+                if (noise < .6f)
+                {
+                    map[i, j] = (int)TileType.WATER;
+                }
+
+                else if (noise < .7f)
+                {
+                    map[i, j] = (int)TileType.SAND;
+                }
+
+
+                else if (noise >= .7f)
+                {
+                    map[i, j] = (int)TileType.GRASS;
+                }
+            }
+        }
+    }
+
+
+
+
+
+    void GenerateGameObjects()
     {
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                UI.CreatePanel("(" + i + ", " + j + ")", sprites[tileMap.map[i, j]], colors[tileMap.map[i, j]], canvas, Vector3.zero,
-                   new Vector2((1.0f / width) * i, (1.0f / height) * j), new Vector2((1.0f / width) * (i + 1), (1.0f / height) * (j + 1)));
+                Vector2 tilePos = new Vector2(i, j);
+
+                int id = map[i, j];
+                GameObject Tile = new GameObject(tileNames[id]);
+               SpriteRenderer sR = Tile.AddComponent<SpriteRenderer>();
+                sR.sprite = sprites[id];
+                sR.color = colors[id];
+                Tile.transform.position = tilePos;
+                Tile.transform.parent = transform;
+
+
+
+                switch (map[i, j])
+                {
+                    case (int)TileType.WATER:
+                        //Change Sprite
+
+                        //Move parts out, only have switch for gameObject
+                        break;
+
+
+                    case (int)TileType.GRASS:
+                        break;
+
+                    default:
+                        
+                        break;
+                }
+                
             }
         }
     }
--- Pirates/Assets/Scripts/TileMap.cs
+++ Pirates/Assets/Scripts/TileMap.cs
@@ -1,27 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+
+public enum TileType
+{
+    WATER,
+    GRASS,
+    SAND,
+    TREE,
+    ROCK,
+    CLIFFS
+}
+
 
 public class TileMap {
-    public GameObject[] tiles;
+    //public GameObject[] tiles;
     private int width;
     private int height;
-    private int uniqueElements;
     public int[,] map;
-    public TileMap(int width, int height,int uniqueElements)
+    public TileMap(int width, int height, float frequency)
     {
         this.width = width;
         this.height = height;
-        this.uniqueElements = uniqueElements;
         map = new int[width, height];
     }
 	
-    public void Generate()
+    public void Generate(float frequency)
     {
+
+
+
         for(int i = 0; i < width; ++i)
         {
             for(int j = 0; j < height; ++j)
             {
-                map[i, j] = Random.Range(0, uniqueElements);
+                //Make this less random
+                float x = (float)i * frequency/ 1000f;
+                float y = (float)j * frequency/ 1000f;
+                float noise = Mathf.PerlinNoise(x, y);
+
+                if (noise < .6f)
+                {
+                    map[i, j] = (int)TileType.WATER;
+                }
+
+                else if (noise < .7f)
+                {
+                    map[i, j] = (int)TileType.SAND;
+                }
+
+
+                else if (noise >= .7f)
+                {
+                    map[i, j] = (int)TileType.GRASS;
+                }
             }
         }
     }
