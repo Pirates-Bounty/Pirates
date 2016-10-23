7e2592b9ccef7ed0f8986e06c4cc7528102d39e6
dcockerham <dylantchaos@gmail.com>
2016-10-22 19:44:43 -0700

    Added in 2-player functionality (may have bugs? further testing required).

--- Pirates/Assets/Scripts/basicPlayer.cs
+++ Pirates/Assets/Scripts/basicPlayer.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class basicPlayer : MonoBehaviour {
 
+	public bool playerOne;
+
 	public Rigidbody2D rb;
 
 	public float rotationSpeed; //150.0f
@@ -24,17 +26,21 @@ public class basicPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-			rb.AddForce(Input.GetAxis("Vertical") * transform.up * moveSpeed);
+		if ((playerOne && Input.GetKey(KeyCode.W)) || (!playerOne && Input.GetKey(KeyCode.UpArrow))) {
+			rb.AddForce(transform.up * moveSpeed);
+		}
+		else if ((playerOne && Input.GetKey(KeyCode.S)) || (!playerOne && Input.GetKey(KeyCode.DownArrow))) {
+			rb.AddForce(-transform.up * moveSpeed/4);
 		}
 
-		else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-			rb.AddForce(Input.GetAxis("Vertical") * transform.up * moveSpeed/4);
+		if ((playerOne && Input.GetKey (KeyCode.A)) || (!playerOne && Input.GetKey (KeyCode.LeftArrow))) {
+			transform.Rotate (0, 0, Time.deltaTime * rotationSpeed);
+			rb.velocity = rb.velocity.magnitude * transform.up;
 		}
-
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
-		transform.Rotate(0, 0, -x);
-		rb.velocity = rb.velocity.magnitude * transform.up;
+		else if ((playerOne && Input.GetKey (KeyCode.D)) || (!playerOne && Input.GetKey (KeyCode.RightArrow))) {
+			transform.Rotate (0, 0, -Time.deltaTime * rotationSpeed);
+			rb.velocity = rb.velocity.magnitude * transform.up;
+		}
 	}
 
 	void Update () {
@@ -42,10 +48,10 @@ public class basicPlayer : MonoBehaviour {
 		// you can hold the fire button down to fire at regular intervals, or tap it and fire automatically at the next interval
 		if (firingTimer > 0) {
 			firingTimer -= Time.deltaTime;
-			if (!loadedBullet && (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.F))) {
+			if (!loadedBullet && ((playerOne && Input.GetKeyDown (KeyCode.Space)) || (!playerOne && Input.GetKeyDown (KeyCode.F)))) {
 				loadedBullet = true;
 			}
-		} else if (Input.GetKey (KeyCode.Space) || Input.GetKey (KeyCode.F) || loadedBullet) {
+		} else if ((playerOne && Input.GetKeyDown (KeyCode.Space)) || (!playerOne && Input.GetKeyDown (KeyCode.F)) || loadedBullet) {
 			FireCannons ();
 			firingTimer = firingDelay;
 			loadedBullet = false;
