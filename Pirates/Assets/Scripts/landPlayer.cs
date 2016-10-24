5afde014e812e5783c80baa78a6881ca39445f27
Tony Phan <phantj@uci.edu>
2016-10-24 14:16:57 -0700

    Player sprite now changes based on direction you are moving!

--- Pirates/Assets/Scripts/landPlayer.cs
+++ Pirates/Assets/Scripts/landPlayer.cs
@@ -3,28 +3,43 @@ using System.Collections;
 
 public class landPlayer : MonoBehaviour {
 
+	public Sprite upSprite;
+	public Sprite leftSprite;
+	public Sprite rightSprite;
+	public Sprite downSprite;
+
+	private SpriteRenderer spriteRenderer;
+
 	public Rigidbody2D rb;
 	public float runSpeed;
 
 	// Use this for initialization
 	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
 		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
 		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-			//display up sprite
+			if (spriteRenderer.sprite != upSprite) {
+				spriteRenderer.sprite = upSprite;
+				//display up sprite
+			}
 		}
 		else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			spriteRenderer.sprite = leftSprite;
 			//display left sprite
 		}
 		else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-			//display down sprite
+			if (spriteRenderer.sprite != downSprite) {
+				spriteRenderer.sprite = downSprite;
+				//display down sprite
+			}
 		}
-		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {			
+		else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+			spriteRenderer.sprite = rightSprite;
 			//display right sprite
 		}
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * runSpeed;
