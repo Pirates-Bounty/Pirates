65d9e9b604fa3da97fcfc21726c70c864592e340
Quinn Levine <Qlevine@uci.edu>
2017-01-29 11:39:11 -0800

    Fixed map problem not showing up on client.  Tested it and it works.  We should test more but I believe its fixed.

--- Pirates/Assets/Scripts/MapGenerator.cs
+++ Pirates/Assets/Scripts/MapGenerator.cs
@@ -45,10 +45,10 @@ public class MapGenerator : MonoBehaviour {
         {
             Destroy(gameObject);
         }
-        if (GameObject.FindGameObjectsWithTag("mapGen").Length >= 2)
-        {
-            Destroy(gameObject);
-        }
+        //if (GameObject.FindGameObjectsWithTag("mapGen").Length >= 2)
+        //{
+        //    Destroy(gameObject);
+        //}
 
             
             Generate();
