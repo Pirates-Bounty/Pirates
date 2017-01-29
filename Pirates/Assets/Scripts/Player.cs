230213d2968a48ddc1c510b1dc8f57c162eedc58
Billy Ismanto <bismanto@uci.edu>
2017-01-28 16:22:38 -0800

    Highlight Button upgrade sound

--- Pirates/Assets/Scripts/Player.cs
+++ Pirates/Assets/Scripts/Player.cs
@@ -351,6 +351,8 @@ public class Player : NetworkBehaviour {
         UpdateInterface();
         UpdateVariables();
         //CmdDisplayHealth ();
+
+        GameObject.Find("SoundManager").transform.position = GameObject.Find("Camera").transform.position;
     }
 
 
@@ -701,6 +703,11 @@ public class Player : NetworkBehaviour {
                 Vector3.zero, new Vector2(0.1f, 1.0f / (int)Upgrade.COUNT * i), new Vector2(0.5f, 1.0f / (int)Upgrade.COUNT * (i + 1)), TextAnchor.MiddleCenter, true);
 			costTexts[i] = UI.CreateText("Cost Text " + i, UPGRADE_COST + "g", font, Color.black, 24, upgradeMenu.transform,
 				Vector3.zero, new Vector2(0.75f, 1.0f / (int)Upgrade.COUNT * i), new Vector2(0.95f, 1.0f / (int)Upgrade.COUNT * (i + 1)), TextAnchor.MiddleCenter, true);
+            //Highlight Sound
+            UnityEngine.EventSystems.EventTrigger.Entry entry_highlight = new UnityEngine.EventSystems.EventTrigger.Entry(); //entry object creation
+            entry_highlight.eventID = UnityEngine.EventSystems.EventTriggerType.PointerEnter; //setting the trigger type; how is it triggered
+            entry_highlight.callback.AddListener((data) => SoundManager.Instance.PlaySFX(GameObject.Find("SoundManager").GetComponent<SoundManager>().highlightAudio)); //call function=> playAudio(...)
+            upgradePlusButton.AddComponent<UnityEngine.EventSystems.EventTrigger>().triggers.Add(entry_highlight);
         }
 		//UpdateVariables ();
         upgradeMenu.SetActive(upgradeMenuActive);
