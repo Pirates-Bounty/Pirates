e78985e2b9c96e197d96d54bd275fefb6a2d203f
Tony <phantj@uci.edu>
2017-04-23 12:21:08 -0700

    working on login

---
+++ Pirates/Assets/Scripts/Login.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login : MonoBehaviour {
+
+    private Transform canvas;
+    private Font font;
+    private Sprite sprite;
+    private Sprite highlightedSprite;
+	private GameObject skipButton;
+    private Color color = Color.black;
+    private int fontSize = 20;
+
+	// Use this for initialization
+	void Start () {
+		playButton = UI.CreateButton("Skip", "Skip", font, color, fontSize, canvas, sprite, highlightedSprite,
+        Vector3.zero, new Vector2(0.05f, 0.05f), new Vector2(0.325f, 0.2f),
+        delegate {
+            Navigator.Instance.LoadLevel("Menu");
+        } );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
