1b2555052b5335b156d2f7d961471bf020b72aa9
Tony <phantj@uci.edu>
2017-04-23 12:37:43 -0700

    fixed bugs in login code

--- Pirates/Assets/Scripts/Login.cs
+++ Pirates/Assets/Scripts/Login.cs
@@ -13,11 +13,11 @@ public class Login : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		playButton = UI.CreateButton("Skip", "Skip", font, color, fontSize, canvas, sprite, highlightedSprite,
-        Vector3.zero, new Vector2(0.05f, 0.05f), new Vector2(0.325f, 0.2f),
-        delegate {
-            Navigator.Instance.LoadLevel("Menu");
-        } );
+		// playButton = UI.CreateButton("Skip", "Skip", font, color, fontSize, canvas, sprite, highlightedSprite,
+  //       Vector3.zero, new Vector2(0.05f, 0.05f), new Vector2(0.325f, 0.2f),
+  //       delegate {
+  //           Navigator.Instance.LoadLevel("Menu");
+  //       } );
 	}
 	
 	// Update is called once per frame
