60eb53d1aa8f8fbfa9b06d3ff2741f1dfa8953a1
Quinn <Qlevine20@yahoo.com>
2017-06-04 14:09:44 -0700

    I upped the base move speed of the player, I fixed the player spawning on land and changed the default width and height of the map to be smaller.

--- Pirates/Assets/Scripts/Player.cs
+++ Pirates/Assets/Scripts/Player.cs
@@ -30,7 +30,7 @@ public class Player : NetworkBehaviour {
 
 	public static float[] SCALE_MAX_HEALTH = { 100f, 150f, 250f, 400f };
 	public static float[] SCALE_RAM_DAMAGE = { 15f, 20f, 30f, 50f };
-	public static float[] SCALE_MOVE_SPEED = { 20f, 30f, 40f, 50f };
+	public static float[] SCALE_MOVE_SPEED = { 50f, 60f, 70f, 80f };
 	public static float[] SCALE_ROTATION_SPEED = { 50f, 50f*4f/3f, 50f*5f/3f, 70f };
 	public static float[] SCALE_BOOST_LENGTH = { 1.0f, 1.25f, 1.5f, 1.75f };
 	public static float[] SCALE_BOOST_DELAY = { 5.0f, 4.0f, 3.0f, 2.0f };
